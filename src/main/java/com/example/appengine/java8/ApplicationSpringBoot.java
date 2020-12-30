3234fc85dc4c94a3412db934207c65e788e3c267
DenysKozii <denys.kozii@gmail.com>
2020-12-31 01:15:24 +0200

    change pom

--- src/main/java/com/example/appengine/java8/ApplicationSpringBoot.java
+++ src/main/java/com/example/appengine/java8/ApplicationSpringBoot.java
@@ -1,13 +1,13 @@
-//package com.example.appengine.java8;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class ApplicationSpringBoot {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(ApplicationSpringBoot.class, args);
-//    }
-//
-//}
+package com.example.appengine.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSpringBoot.class, args);
+    }
+
+}
